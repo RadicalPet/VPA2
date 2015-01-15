13c161ad666047221c3323f115362969de3b851d
RadicalPet <sarah@radicalpet.net>
2015-01-15 05:00:42 +0100

    MORE crypto (python, c#)

--- VPA2/Controllers/ClientsController.cs
+++ VPA2/Controllers/ClientsController.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
-using System.Web;
+using System.Text;
 using System.Web.Mvc;
 using VPA2.Models;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.Crypto.Encodings;
+
+
 
 namespace VPA2.Controllers
 {
@@ -23,6 +29,7 @@ namespace VPA2.Controllers
         // GET: Clients/Details/5
         public ActionResult Details(int? id)
         {
+           
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
@@ -32,6 +39,43 @@ namespace VPA2.Controllers
             {
                 return HttpNotFound();
             }
+            var client = new RestClient("http://178.62.247.139:9002");
+            var request = new RestRequest(Method.POST);
+                request.AddHeader("Content-Type", "application/json");
+                request.RequestFormat = DataFormat.Json;
+
+                request.AddParameter("firstname", clients.firstName);
+                request.AddParameter("lastname", clients.lastName);
+                request.AddParameter("email", clients.email);
+                request.AddParameter("message", clients.message);
+
+                RestResponse response = (RestResponse)client.Execute(request);
+                var responseString = response.Content; 
+                // raw content as string
+                if (!string.IsNullOrEmpty(responseString))
+                {
+                    var responseObject = JsonConvert.DeserializeObject<Clients>(responseString);
+
+                    var bytesFirstName = Convert.FromBase64String(responseObject.firstName);
+                    var bytesLastName = Convert.FromBase64String(responseObject.lastName);
+                    var bytesEmail = Convert.FromBase64String(responseObject.email);
+                    var bytesMessage = Convert.FromBase64String(responseObject.message);
+
+                    AsymmetricCipherKeyPair keyPair;
+
+                    using (var reader = System.IO.File.OpenText(@"D:\jagenau\Documents\Visual Studio 2013\Projects\VPA2\VPA2\Assets\pyKey.pem")) 
+                    // file containing RSA PKCS1 private key
+                    keyPair = (AsymmetricCipherKeyPair)new PemReader(reader).ReadObject();
+
+                    var decryptEngine = new Pkcs1Encoding(new RsaEngine());
+                    decryptEngine.Init(false, keyPair.Private);
+
+                    clients.firstName =  Encoding.UTF8.GetString(decryptEngine.ProcessBlock(bytesFirstName, 0, bytesFirstName.Length));
+                    clients.lastName = Encoding.UTF8.GetString(decryptEngine.ProcessBlock(bytesLastName, 0, bytesLastName.Length));
+                    clients.email = Encoding.UTF8.GetString(decryptEngine.ProcessBlock(bytesEmail, 0, bytesEmail.Length));
+                    clients.message = Encoding.UTF8.GetString(decryptEngine.ProcessBlock(bytesMessage, 0, bytesMessage.Length));                    
+                }
+               
             return View(clients);
         }
 
